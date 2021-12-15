1f55fc5b274802729563ef6936cd7f7768076137
fanfan <2284121004@qq.com>
2021-12-15 16:58:49 +0800

    完成header和footer

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -4,6 +4,8 @@ import { Layout } from 'antd';
 import { connect } from 'umi';
 import logo from '../imgs/brand.png';
 import styles from './index.less';
+import digitalocean from '../imgs/digitalocean.png';
+import qiniu from '../imgs/qiniu.png';
 
 const Header = Layout.Header;
 const Content = Layout.Content;
@@ -16,25 +18,76 @@ function BasicLayout({ children }: BasicProps) {
   return (
     <Layout>
       <Header className={styles.header}>
-        <div className="navbar-inner">
-          <div className="brand">
-            <a href="#">
-              123
-              <img width="50" src={logo} alt="" />
-            </a>
+        <div className={styles.navbar}>
+          <div className={styles.navbarLeft}>
+            <div className={styles.brand}>
+              <a href="#">
+                <img width="120px" src={logo} alt="" />
+              </a>
+            </div>
+            <div className={styles.headerSearch}>
+              <form action="" className={styles.headerSearch}>
+                <input type="text" />
+              </form>
+            </div>
           </div>
-          <div className="header-search">
-            <form action="">
-              <input type="text" />
-            </form>
+          <div className={styles.navbarRight}>
+            <ul>
+              <li>
+                <a>首页</a>
+              </li>
+              <li>
+                <a>新手入门</a>
+              </li>
+              <li>
+                <a>API</a>
+              </li>
+              <li>
+                <a>关于</a>
+              </li>
+              <li>
+                <a>注册</a>
+              </li>
+              <li>
+                <a>登录</a>
+              </li>
+            </ul>
           </div>
-          <div className="am-navbar-right"></div>
         </div>
       </Header>
       <Layout>
         <Content>{children}</Content>
       </Layout>
-      <Footer>footer</Footer>
+      <Footer className={styles.Footer}>
+        <div className={styles.footMain}>
+          <div className={styles.links}>
+            <a href="">RSS</a> | <a href="">源码地址</a>
+          </div>
+          <div className={styles.colFoots}>
+            <p>
+              CNode 社区为国内最专业的 Node.js 开源技术社区，致力于 Node.js
+              的技术研究。
+            </p>
+            <p>
+              <p>服务器搭建在 </p>
+              <a href="#">
+                <img src={digitalocean} width="92" alt="" />
+              </a>
+              <p> , 存储赞助商为 </p>
+              <a href="#">
+                <img src={qiniu} alt="" width="115" />
+              </a>
+            </p>
+            <p>
+              新手搭建 Node.js 服务器，推荐使用无需备案的
+              <a href="https://www.digitalocean.com/">
+                {' '}
+                DigitalOcean(https://www.digitalocean.com/)
+              </a>
+            </p>
+          </div>
+        </div>
+      </Footer>
     </Layout>
   );
 }
